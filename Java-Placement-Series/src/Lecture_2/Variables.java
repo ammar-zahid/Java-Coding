e858e2dee648e301345fa529e50e8fb6da9915bb
Ammar Zahid <ammarzahid304@gmail.com>
2023-04-21 17:08:43 +0500

    Variables in Java

---
+++ Java-Placement-Series/src/Lecture_2/Variables.java
@@ -0,0 +1,18 @@
+package Lecture_2;
+
+public class Variables {
+    public static void main(String[] args) {
+        
+        int number = 5;
+        double percentage = 93.5;
+        char character = '$';
+        String name = "Tony Stark";
+        Boolean decision = true;
+
+        System.out.println(number);
+        System.out.println(percentage);
+        System.out.println(character);
+        System.out.println(name);
+        System.out.println(decision);
+    }
+}
