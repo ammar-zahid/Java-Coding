def39bb2b279801e0e5bdbd75b0bd1c861597b52
Ammar Zahid <ammarzahid304@gmail.com>
2023-04-21 17:16:00 +0500

    Simple calculations

---
+++ Java-Placement-Series/src/Lecture_2/BasicMaths.java
@@ -0,0 +1,18 @@
+package Lecture_2;
+
+public class BasicMaths {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 7;
+
+        int addition = x + y;
+        int subtraction = y - x;
+        int multiplication = x * y;
+        int division = y / x;
+
+        System.out.println(addition);
+        System.out.println(subtraction);
+        System.out.println(multiplication);
+        System.out.println(division);
+    }
+}
