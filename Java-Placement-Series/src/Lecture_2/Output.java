080b888642f1928485d7ccc8ebbc2ae67bca1c10
Ammar Zahid <ammarzahid304@gmail.com>
2023-04-21 17:01:31 +0500

    Output statement

---
+++ Java-Placement-Series/src/Lecture_2/Output.java
@@ -0,0 +1,16 @@
+package Lecture_2;
+
+public class Output {
+    public static void main(String[] args) {
+        // to print output on screen
+        System.out.println("I am learning Java");
+        System.out.println("from Java Placement Course");
+
+        // to print output on same line
+        System.out.print("I am learning Java ");
+        System.out.println("from Java Placement Course");
+
+        // another method for next line
+        System.out.println("I am learning Java\nfrom Java Placement Course");
+    }
+}
