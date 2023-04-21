f2c1f69bda648e187bf81fd3f05b2f01aec9ea8f
Ammar Zahid <ammarzahid304@gmail.com>
2023-04-21 21:46:16 +0500

    Homework Qno1

---
+++ Java-Placement-Series/src/Lecture_2/HomeWork_Problems/Qno_1.java
@@ -0,0 +1,19 @@
+// Try to declare meaningful variables of each type. Eg - a variable named age should be a numeric type (int or float) not byte.
+
+package Lecture_2.HomeWork_Problems;
+
+public class Qno_1 {
+    public static void main(String[] args) {
+        int age = 19;
+        char symbol = '@';
+        String name = "Ammar";
+        double percentage = 96.43;
+        Boolean decision = true;
+
+        System.out.println(age);
+        System.out.println(symbol);
+        System.out.println(name);
+        System.out.println(percentage);
+        System.out.println(decision);
+    }
+}
