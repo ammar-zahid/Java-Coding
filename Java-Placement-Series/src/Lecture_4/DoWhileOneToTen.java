7c607d703ba743cdbf965d898205e708db4a44f9
Ammar Zahid <ammarzahid304@gmail.com>
2023-04-23 11:26:01 +0500

    Do while loop

---
+++ Java-Placement-Series/src/Lecture_4/DoWhileOneToTen.java
@@ -0,0 +1,12 @@
+package Lecture_4;
+
+public class DoWhileOneToTen {
+    public static void main(String[] args) {
+       
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 11);
+    }
+}
