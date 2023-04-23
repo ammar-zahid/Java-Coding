2496e5347b87b7517e28531220299d594d9bdce7
Ammar Zahid <ammarzahid304@gmail.com>
2023-04-23 11:21:26 +0500

    For loop

---
+++ Java-Placement-Series/src/Lecture_4/ForOneToTen.java
@@ -0,0 +1,9 @@
+package Lecture_4;
+
+public class ForOneToTen {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);
+        }
+    }
+}
