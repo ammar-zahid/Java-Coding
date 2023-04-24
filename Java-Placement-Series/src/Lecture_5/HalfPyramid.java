c72bde1cd103d5938b52d8eafda8e9d8f55ef3e3
Ammar Zahid <ammarzahid304@gmail.com>
2023-04-24 15:25:19 +0500

    Pattern Qno3

---
+++ Java-Placement-Series/src/Lecture_5/HalfPyramid.java
@@ -0,0 +1,14 @@
+package Lecture_5;
+
+public class HalfPyramid {
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 1; j <= i + 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
