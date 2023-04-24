5871978b5ee6073e1e13a5a2cfe11ee80ce529a9
Ammar Zahid <ammarzahid304@gmail.com>
2023-04-24 15:44:39 +0500

    Pattern Qno5

---
+++ Java-Placement-Series/src/Lecture_5/HalfPyramid180deg.java
@@ -0,0 +1,17 @@
+package Lecture_5;
+
+public class HalfPyramid180deg {
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5 - i; j++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
