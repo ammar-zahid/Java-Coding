a32828aadd8bbadb7cefedf412893e0e09df70ee
Ammar Zahid <ammarzahid304@gmail.com>
2023-04-26 23:10:38 +0500

    Average of numbers

---
+++ Mids-Preparation/src/Codes/AverageOfNumbers.java
@@ -0,0 +1,29 @@
+// Enter 3 numbers from the user & make a function to print their average.
+
+package Codes;
+
+import java.util.Scanner;
+public class AverageOfNumbers {
+    public static double Average(int x, int y, int z){
+        int avg;
+        avg = (x + y + z) / 3;
+        return avg;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter x: ");
+        int x = sc.nextInt();
+
+        System.out.print("Enter y: ");
+        int y = sc.nextInt();
+
+        System.out.print("Enter z: ");
+        int z = sc.nextInt();
+
+        double answer = Average(x, y, z);
+
+        System.out.println("Average is " + answer);
+        sc.close();
+    }
+}
