02b9d5866ded35f219a2e3254f0cb08f3bbf766e
Ammar Zahid <ammarzahid304@gmail.com>
2023-04-26 19:44:00 +0500

    Function to check prime number

---
+++ Java-Placement-Series/src/Lecture_7/HomeWork_Problems/Qno_1.java
@@ -0,0 +1,27 @@
+// Make a function to check if a number is prime or not
+package Lecture_7.HomeWork_Problems;
+
+import java.util.*;
+public class Qno_1 {
+    public static void PrimeNumber(int n){
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                count = count + 1;
+            }
+        }
+        if(count == 2){
+        System.out.println("Number is prime");
+        } else {
+            System.out.println("Number is not prime");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        PrimeNumber(n);
+    }
+}
