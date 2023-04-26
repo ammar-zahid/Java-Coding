17cb0aa5ba29f13c03b14978fe358af94e652476
Ammar Zahid <ammarzahid304@gmail.com>
2023-04-26 23:18:01 +0500

    Function to calculate sum of odd numbers

---
+++ Mids-Preparation/src/Codes/SumOfOddNumbersFunction.java
@@ -0,0 +1,28 @@
+// Write a function to print the sum of all Odd numbers from 1 to n
+
+package Codes;
+import java.util.Scanner;
+
+public class SumOfOddNumbersFunction {
+    public static int Sum(int n){
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+                sum = sum + i;
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        int answer = Sum(n);
+
+        System.out.println("Sum of odd numbers from 1 to n is : " + answer);
+        sc.close();
+    }
+}
