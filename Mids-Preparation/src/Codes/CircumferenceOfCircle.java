be2d8799c75bdb55ac95824268e105a1819b87af
Ammar Zahid <ammarzahid304@gmail.com>
2023-04-27 10:02:20 +0500

    function to find circumference of circle

---
+++ Mids-Preparation/src/Codes/CircumferenceOfCircle.java
@@ -0,0 +1,22 @@
+// Write a function that takes in the radius as input and returns the circumference of a circle.
+package Codes;
+import java.util.Scanner;
+
+public class CircumferenceOfCircle {
+    public static double Circumference(double radius){
+        double circumference = 2 * 3.14 * radius;
+        return circumference;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter radius: ");
+        double radius = sc.nextDouble();
+
+        double answer = Circumference(radius);
+
+        System.out.println("Circumference of circle is " + answer);
+        sc.close();
+    }
+}
