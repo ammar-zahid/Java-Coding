ec1fc70f6a5b930fad333f479c71837d44666cff
Ammar Zahid <ammarzahid304@gmail.com>
2023-04-27 21:13:30 +0500

    Leap year checker

---
+++ Mids-Preparation/src/Codes/LeapYearChecker.java
@@ -0,0 +1,19 @@
+// Write a program to check whether a year entered by user is leap year or not 
+
+package Codes;
+import java.util.Scanner;
+
+public class LeapYearChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a year: ");
+        int year = sc.nextInt();
+
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+        sc.close();
+    }
+}
