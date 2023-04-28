267f43d7269fcbd7f0afa6a129b9ae2164f1bb15
Ammar Zahid <ammarzahid304@gmail.com>
2023-04-28 21:21:31 +0500

    Qno6 of Homework Exercise

---
+++ Java-Placement-Series/src/Lecture_8/InfiniteDoWhileLoop.java
@@ -0,0 +1,11 @@
+// write an infinite loop using do while condition
+
+package Lecture_8;
+
+public class InfiniteDoWhileLoop {
+    public static void main(String[] args) {
+        do {
+            System.out.println("Hello");
+        } while (true);
+    }
+}
