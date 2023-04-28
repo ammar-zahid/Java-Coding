83b87fd2d074f16bb475e82f10cc26ee47727340
Ammar Zahid <ammarzahid304@gmail.com>
2023-04-28 21:10:57 +0500

    Qno3 of Homework Exercise

---
+++ Java-Placement-Series/src/Lecture_8/ReturnGreaterNumberFunction.java
@@ -0,0 +1,29 @@
+// Write a function which takes in 2 numbers and returns the greater of those two.
+
+package Lecture_8;
+
+import java.util.Scanner;
+public class ReturnGreaterNumberFunction {
+    public static void GreaterNumber(int x, int y){
+        if (x > y) {
+            System.out.println(x + " is greater than " + y);
+        } else if (y > x) {
+            System.out.print(y + " is greater than " + x);
+        } else {
+            System.out.println(x + " and " + y + " are equal");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter x: ");
+        int x = sc.nextInt();
+
+        System.out.print("Enter y: ");
+        int y = sc.nextInt();
+
+        GreaterNumber(x, y);
+        sc.close();
+    }
+}
